2e495e49955ea833bc937844fc9e95898691a90e
Jordan Gumm <gumm1jn@cmich.edu>
2015-06-01 16:25:53 -0700

    added apps to setting and default url to path

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'beta',
+    'news',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -66,6 +68,8 @@ DATABASES = {
     }
 }
 
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
@@ -84,3 +88,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    './../static/',
+)
