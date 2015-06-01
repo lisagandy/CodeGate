248fbebb671c64e2291a0370b78c7c8b059eacb8
Jordan Gumm <gumm1jn@cmich.edu>
2015-06-01 16:24:16 -0700

    added demo deflategate hmtl and view

--- mysite/beta/views.py
+++ mysite/beta/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import RequestContext, loader
 
-# Create your views here.
+from news.models import NewsArticle
+
+
+def index(request):
+    latest_news_list = NewsArticle.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('beta/deflategate.html')
+    context = RequestContext(request, {
+        'latest_news_list': latest_news_list,
+    })
+    
+    print latest_news_list
+    return HttpResponse(template.render(context))
