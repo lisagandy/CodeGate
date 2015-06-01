1e1923494130dacaff9db08e2ae1000678b7cf18
Jordan Gumm <gumm1jn@cmich.edu>
2015-06-01 16:23:11 -0700

    updated mysite models to eliminate errors when migrating

--- mysite/beta/models.py
+++ mysite/beta/models.py
@@ -1,36 +1,37 @@
 from django.db import models
 
-# Create your models here.
-class GateEvent:
-    name = models.CharField()
 
-class Tweet:
-    
-    gateKey = models.ForeignKey(GateEvent)
-    tweetID = models.CharField()
-    twitterUserKey = models.ForeignKey(TwitterUser)
-    tweetText = models.TextField()
-    tweetTime = models.DateTimeField()
-    numRetweets = models.IntegerField(default=0)
-    hasURL = models.BooleanField()
-    hasHashTag = models.BooleanField()
-    hasRetweet = models.BooleanField()
-    tweetPlace = models.CharField()
-    
-class URL:
-    
-    url = models.URLField()    
-    tweetKeys = models.ManyToManyField(Tweet)
-    
-class HashTag:
-    hashtag = models.CharField()
-    tweetKey = models.ManyToManyField(Tweet)
-    
-class TwitterUser:
-    
-    userID = models.CharField()
-    userName = models.CharField()
+class GateEvent(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class TwitterUser(models.Model):    
+    userID = models.CharField(max_length=15)
+    userName = models.CharField(max_length=15)
     profileURL = models.URLField()
-    location = models.CharField()
+    location = models.CharField(max_length=30)
     numFollowers = models.IntegerField()
+
+
+class Tweet(models.Model):
+    gateKey = models.ForeignKey(GateEvent)
+    tweetID = models.CharField(max_length=15)
+    twitterUserKey = models.ForeignKey(TwitterUser)
+    tweetText = models.CharField(max_length=140)
+    tweetTime = models.DateTimeField()
+    numRetweets = models.IntegerField(default=False)
+    hasURL = models.BooleanField(default=False)
+    hasHashTag = models.BooleanField(default=False)
+    hasRetweet = models.BooleanField(default=False)
+    tweetPlace = models.CharField(max_length=30)
+
+
+class URL(models.Model):
+    url = models.URLField()
+    tweetKeys = models.ManyToManyField(Tweet)
+
+
+class HashTag(models.Model):
+    hashtag = models.CharField(max_length=20)
+    tweetKey = models.ManyToManyField(Tweet)
     
